284a7193c6b24a61f3f53c63bf71b48ca3fa465d
Rifsha <rifsha.ashraf1@gmail.com>
2023-04-12 03:29:45 +0500

    second committt

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
+
 var mini = true;
         function toggleSidebar() {
             if (mini) {
-                console.log("opening sidebar");
-                document.getElementById("mySidebar").style.width = "210px";
-                document.getElementById("main").style.marginRight = "210px";
+                console.log("opening nav");
+                document.getElementById("nav-bar").style.width = "210px";
                 mini = false;
             } 
             else {
-                console.log("closing sidebar");
-                document.getElementById("mySidebar").style.width = "85px";
-                document.getElementById("main").style.marginRight = "205px";
+                console.log("closing nav");
+                document.getElementById("nav-bar").style.width = "5rem";
                 mini = true;
             }
         }
         (function () {
-            [...document.querySelectorAll(".control")].forEach(button => {
+            [...document.querySelectorAll(".nav")].forEach(button => {
                 button.addEventListener("click", function() {
                     document.querySelector(".active-btn").classList.remove("active-btn");
                     this.classList.add("active-btn");
